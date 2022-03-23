badcc8598fef90c0f5ce500a346b41e05ab4dc74
Andrew <andrewelder18@gmail.com>
2022-03-23 12:03:13 -0400

    Added in struct and initial stuff

--- laba4.c
+++ laba4.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int N;
-int M;
+int N; //Number of virtual pages 
+int M; //Number of actual pages (page frame)
 int buffer[100]; 
+int refStringSize = 0;
+
+typedef struct Page 
+{
+	int refernce;
+	struct Page* next;
+	struct Page* prev;
+} Page;
+
+Page* pageList = NULL;
 
 void openSample()
 {
@@ -29,6 +39,7 @@ void openSample()
 		buffer[counter] = temp;
 		counter++;
 	}
+	refStringSize = counter-1;
 	printf("Size of refernce string: %d\n", counter-1);
 	printf("Refernce String: \n");
 	for(int i = 0; i < counter-1; i++)
@@ -38,9 +49,49 @@ void openSample()
 	printf("\n\n");
 }
 
+void initList ()
+{
+	Page* pageListRef = NULL;
+	pageList = malloc(sizeof(Page));
+	pageList->refernce = -1;
+	pageList->prev = NULL;
+	pageList->next = pageListRef;
+	
+	Page* pageListPrev = pageList;
+	
+	for(int i = 0; i < M; i++)
+	{
+		pageListRef = malloc(sizeof(Page));
+		pageListRef->refernce = -1;
+		pageListRef->prev = pageListPrev;
+		pageListRef->next = NULL;
+		
+		pageListPrev->next = pageListRef;
+		pageListPrev = pageListRef;
+	}
+}
+
+void ptrList()
+{
+	Page* curr = pageList;
+	while(curr->next != NULL)
+	{
+		printf("Refernce: %d \n", curr->refernce);
+		curr = curr->next;
+	}
+}
+
 void fifo()
 {
-	printf("\nFifo");
+	//printf("\nFifo");
+	for(int i = 0; i < refStringSize; i++)
+	{
+	
+	
+	
+	
+	
+	}
 	
 }
 
@@ -61,6 +112,9 @@ void leastUsed()
 int main()
 {
 	openSample();
+	initList();
+	ptrList();
+	
 	fifo();
 	return 0;
 }
